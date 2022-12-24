00772b6594116b221a8b4d9db98f6fcf9f04846a
MeghanaReddy2005 <119720532+MeghanaReddy2005@users.noreply.github.com>
2022-12-24 19:58:09 +0530

    #include<stdio.h> int main() {     int n,q,r,s=0;     scanf("%d",&n);     q=n;     while(q!=0)     {     r=q%10;     q=q/10;     s=s*10+r;     }     if(s==n)     {     printf("True");     }      else         {             printf("False");         } }

--- Palindrome.c
+++ Palindrome.c
@@ -6,16 +6,16 @@ int main()
     q=n;
     while(q!=0)
     {
-        r=q%10;
-        q=q/10;
-        s=s*10+r;
+    r=q%10;
+    q=q/10;
+    s=s*10+r;
     }
     if(s==n)
     {
-        printf("True");
+    printf("True");
     }
-    else
-    {
-        printf("False");
-    }
+     else
+        {
+            printf("False");
+        }
 }
