a4af5a72310c379c0dd2bc8bf2458e83bb2a0915
MeghanaReddy2005 <119720532+MeghanaReddy2005@users.noreply.github.com>
2022-12-31 11:22:12 +0530

    # include <stdio.h>
    int main() {
    
        int n, i = 0;
        scanf("%d",&n);
        while(i < 12)
        {
            i++;
            printf("%d x %d = %d
    ", n, i, n * i);
        }
    
        return 0;
    }

---
+++ Multiplication_Table_1.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main() {
+
+    int n, i = 0;
+    scanf("%d",&n);
+    while(i < 12)
+    {
+        i++;
+        printf("%d x %d = %d
+", n, i, n * i);
+    }
+
+    return 0;
+}
